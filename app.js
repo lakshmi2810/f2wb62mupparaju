833b7b28efae8d42a8a052dced3c966e70233ac4
lakshmi2810 <35185775+lakshmi2810@users.noreply.github.com>
2022-11-01 16:48:55 -0500

    added images

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var LunchBoxRouter = require('./routes/LunchBox');
 var gridbuildRouter=require('./routes/gridbuild');
-
+var selectorRouter=require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/LunchBox', LunchBoxRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -42,3 +43,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
