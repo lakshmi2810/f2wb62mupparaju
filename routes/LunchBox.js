86f43649f08bf4b4c0dfbea3b0b9fb48cd22fe11
lakshmi2810 <35185775+lakshmi2810@users.noreply.github.com>
2022-11-01 14:32:19 -0500

    class js and pug

---
+++ routes/LunchBox.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.render('LunchBox', { title: ' Search Results for LunchBox' });
+  
+});
+
+module.exports = router;
